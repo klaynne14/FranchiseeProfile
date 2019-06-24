a34da35672a90dbca79fa9e9f4b41777be15f7cf
klaynne14 <49712715+klaynne14@users.noreply.github.com>
2019-06-24 13:16:30 +0800

    Add files via upload
    first commit, inc stuff

---
+++ FranchiseeProf/FranchiseeProf/FranchiseeProf/Add New Branch.vb
@@ -0,0 +1,3 @@
+﻿Public Class Add_New_Branch
+
+End Class
---
+++ FranchiseeProf/FranchiseeProf/FranchiseeProf/CreateNewFranchisee.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CreateNewFranchisee
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CreateNewFranchisee))
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox3 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox4 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox5 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox6 = New System.Windows.Forms.RichTextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Panel4.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel4
+        '
+        Me.Panel4.Controls.Add(Me.DataGridView1)
+        Me.Panel4.Controls.Add(Me.ComboBox1)
+        Me.Panel4.Controls.Add(Me.RichTextBox6)
+        Me.Panel4.Controls.Add(Me.RichTextBox5)
+        Me.Panel4.Controls.Add(Me.RichTextBox4)
+        Me.Panel4.Controls.Add(Me.RichTextBox3)
+        Me.Panel4.Controls.Add(Me.RichTextBox2)
+        Me.Panel4.Controls.Add(Me.RichTextBox1)
+        Me.Panel4.Controls.Add(Me.Label2)
+        Me.Panel4.Controls.Add(Me.Label1)
+        Me.Panel4.Controls.Add(Me.Button3)
+        Me.Panel4.Controls.Add(Me.Button2)
+        Me.Panel4.Controls.Add(Me.Button1)
+        Me.Panel4.Controls.Add(Me.PictureBox3)
+        Me.Panel4.Location = New System.Drawing.Point(375, 15)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(1040, 780)
+        Me.Panel4.TabIndex = 18
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.SkyBlue
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.Transparent
+        Me.Button2.Location = New System.Drawing.Point(890, 82)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(99, 46)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Save Changes"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.SkyBlue
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.Transparent
+        Me.Button1.Location = New System.Drawing.Point(890, 18)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 46)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Edit"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(97, 132)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(128, 128)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 0
+        Me.PictureBox3.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.SkyBlue
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.Transparent
+        Me.Button3.Location = New System.Drawing.Point(97, 470)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(99, 46)
+        Me.Button3.TabIndex = 16
+        Me.Button3.Text = "Add New"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(93, 444)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 23)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Branch"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(31, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(260, 23)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "Create New Franchisee"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.ForeColor = System.Drawing.Color.DimGray
+        Me.RichTextBox1.Location = New System.Drawing.Point(251, 201)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(189, 77)
+        Me.RichTextBox1.TabIndex = 25
+        Me.RichTextBox1.Text = "Address"
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.ForeColor = System.Drawing.Color.DimGray
+        Me.RichTextBox2.Location = New System.Drawing.Point(251, 132)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(189, 26)
+        Me.RichTextBox2.TabIndex = 26
+        Me.RichTextBox2.Text = "First Name"
+        '
+        'RichTextBox3
+        '
+        Me.RichTextBox3.ForeColor = System.Drawing.Color.DimGray
+        Me.RichTextBox3.Location = New System.Drawing.Point(456, 132)
+        Me.RichTextBox3.Name = "RichTextBox3"
+        Me.RichTextBox3.Size = New System.Drawing.Size(146, 26)
+        Me.RichTextBox3.TabIndex = 27
+        Me.RichTextBox3.Text = "Last Name"
+        '
+        'RichTextBox4
+        '
+        Me.RichTextBox4.ForeColor = System.Drawing.Color.DimGray
+        Me.RichTextBox4.Location = New System.Drawing.Point(456, 201)
+        Me.RichTextBox4.Name = "RichTextBox4"
+        Me.RichTextBox4.Size = New System.Drawing.Size(146, 28)
+        Me.RichTextBox4.TabIndex = 28
+        Me.RichTextBox4.Text = "Contact No."
+        '
+        'RichTextBox5
+        '
+        Me.RichTextBox5.ForeColor = System.Drawing.Color.DimGray
+        Me.RichTextBox5.Location = New System.Drawing.Point(635, 132)
+        Me.RichTextBox5.Name = "RichTextBox5"
+        Me.RichTextBox5.Size = New System.Drawing.Size(192, 26)
+        Me.RichTextBox5.TabIndex = 29
+        Me.RichTextBox5.Text = "Email"
+        '
+        'RichTextBox6
+        '
+        Me.RichTextBox6.ForeColor = System.Drawing.Color.DimGray
+        Me.RichTextBox6.Location = New System.Drawing.Point(635, 201)
+        Me.RichTextBox6.Name = "RichTextBox6"
+        Me.RichTextBox6.Size = New System.Drawing.Size(146, 28)
+        Me.RichTextBox6.TabIndex = 30
+        Me.RichTextBox6.Text = "Franchise Term"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.ForeColor = System.Drawing.Color.DimGray
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(251, 296)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(66, 21)
+        Me.ComboBox1.TabIndex = 32
+        Me.ComboBox1.Text = "Status"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(97, 522)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(927, 249)
+        Me.DataGridView1.TabIndex = 33
+        '
+        'CreateNewFranchisee
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1440, 900)
+        Me.Controls.Add(Me.Panel4)
+        Me.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "CreateNewFranchisee"
+        Me.Text = "CreateNewFranchisee"
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents RichTextBox6 As RichTextBox
+    Friend WithEvents RichTextBox5 As RichTextBox
+    Friend WithEvents RichTextBox4 As RichTextBox
+    Friend WithEvents RichTextBox3 As RichTextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
